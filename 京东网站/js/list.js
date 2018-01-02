59317efcd61f8011c818d3dd15987c1a2360ce0a
mayidan01 <1265221680@qq.com>
2018-01-02 08:58:00 +0800

    新增项目

---
+++ 京东网站/js/list.js
@@ -0,0 +1,43 @@
+//图片切换
+ var swiper = new Swiper('.list .dv11 .swiper-container', {
+      slidesPerView: 5,
+      spaceBetween: 50,
+      // init: false,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        }
+      }
+    });
+
+var swiper = new Swiper('.dv12 .swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+ 
+
+
+//
