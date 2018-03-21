57af3d9e696a762ec64e845f9929e0f5a514527a
mayidan01 <1265221680@qq.com>
2018-03-21 16:39:11 +0800

    新增

---
+++ 三星/js/style.js
@@ -0,0 +1,26 @@
+ var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+//
+//$('.expandMore').click(function(){
+//          //    $('#briefInfo').show();
+//          $('.briefInfo').toggle();
+//          });
+
+
+
+
+
+
+
+
